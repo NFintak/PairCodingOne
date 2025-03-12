68b74f968a9a77626b0a449a1345f7d60d934ff8
Nathan <natefintak@gmail.com>
2025-03-12 17:19:33 -0400

    in progress

--- src/main/java/io/zipcoder/PersonAcct.java
+++ src/main/java/io/zipcoder/PersonAcct.java
@@ -3,12 +3,33 @@ package io.zipcoder;
 public class PersonAcct extends Account {
     private String lastName;
     private String email;
-    private String phoneNumber;
+    private Integer phoneNumber;
 
-    public PersonAcct(String acctHolder, Integer acctNum, String lastName, String email, String phoneNumber) {
+    public PersonAcct(String acctHolder, Integer acctNum, String lastName, String email, Integer phoneNumber) {
         super(acctHolder, acctNum);
         this.lastName = lastName;
         this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public PersonAcct(String acctHolder, Integer acctNum, String lastName) {
+        super(acctHolder, acctNum);
+        this.lastName = lastName;
+        this.email = "";
+        this.phoneNumber = null;
+    }
+
+    public PersonAcct(String acctHolder, Integer acctNum, String lastName, String email) {
+        super(acctHolder, acctNum);
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = null;
+    }
+
+    public PersonAcct(String acctHolder, Integer acctNum, String lastName, Integer phoneNumber) {
+        super(acctHolder, acctNum);
+        this.lastName = lastName;
+        this.email = "";
         this.phoneNumber = phoneNumber;
     }
 
@@ -28,11 +49,11 @@ public class PersonAcct extends Account {
         this.email = email;
     }
 
-    public String getPhoneNumber() {
+    public Integer getPhoneNumber() {
         return this.phoneNumber;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
+    public void setPhoneNumber(Integer phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 }
