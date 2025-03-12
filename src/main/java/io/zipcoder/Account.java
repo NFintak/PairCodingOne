46fb45cf86541b12b0acbe7d17523ea962c662f4
Nathan <natefintak@gmail.com>
2025-03-12 10:59:58 -0400

    in progress

--- src/main/java/io/zipcoder/Account.java
+++ src/main/java/io/zipcoder/Account.java
@@ -1,7 +1,18 @@
 package io.zipcoder;
+import java.util.ArrayList;
 
 public abstract class Account {
+    private String acctHolder;
     private double balance;
     private Integer accountNum;
-    private ArrayList<>
+    private ArrayList<String> transactionHistory;
+    private double checkingAcct;
+    private double savingsAcct;
+    private double investmentAcct;
+
+    public Account(String acctHolder, Integer accountNum) {
+        this.acctHolder = acctHolder;
+        this.accountNum = accountNum;
+        this.balance = 0.0;
+    }
 }
